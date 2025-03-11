635166510a05ff1c747c0bfa4624b7d865979243
Iago-Santos-Sousa <iago.santos.sousa@gmail.com>
2025-03-11 01:19:33 -0300

    refactor: update database configuration to use async loading with ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,33 @@
 import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
 import { LoggerMiddleware } from "./common/middleware/logger.middleware";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { DataSource } from "typeorm";
 import { UserModule } from "./user/user.module";
-import { User } from "./user/entities/user.entity";
 import { AuthModule } from "./auth/auth.module";
 import { RolesGuardModule } from "./roles-guard/roles-guard.module";
+import databaseConfig from "./config/database.config";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ".env",
+      load: [databaseConfig],
     }),
 
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_SCHEMA,
-      entities: [User],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const configOptions =
+          configService.get<TypeOrmModuleOptions>("database");
+        if (!configOptions) {
+          throw new Error("Database configuration not found");
+        }
+        return configOptions;
+      },
     }),
     AuthModule,
     RolesGuardModule,
